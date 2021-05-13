79a2c7203cc84e039c7ccdd8183b8fbd3bdd0db8
Mahmoud Anbousi <anbousi@gmail.com>
2021-05-13 13:28:55 +0300

    counter flask modefied

--- python_stack/flask/flask_fundamentals/counter/server.py
+++ python_stack/flask/flask_fundamentals/counter/server.py
@@ -1,43 +1,45 @@
 from flask import Flask , render_template , redirect , request , session
 app = Flask(__name__)
 app.secret_key = "Secret key"
-
+count = 1
 @app.route('/')
 def index():
-    session['add'] = 0
-    session['reset'] = 0
-    print(session['add'] , session['reset'] )
-    if 'counter' in session:
-        session['counter'] += 1
-        print(session['counter'])
+    if 'real_counter' and 'counter' in session:
+        session['counter'] += count
+        session['real_counter'] += 1
+
     else:
         session['counter'] = 1
+        if 'real_counter' not in session:
+            session['real_counter'] = 1
+        session['increment'] = 1
         
-    return render_template("index.html" , count = session['counter'])
+    return render_template("index.html" , count = session['counter'] , count1 = session['increment'] , real_count = session['real_counter'])
 
 
 @app.route('/counter' , methods=['POST'])
 def counters():
-    # session['add'] = request.form['add']
-    # session['reset'] = request.form['reset']
-    # if session['add']:
-    #     print("added")
-    #     session['counter'] += 1
-
-    # if session['reset']:
-    #     # session.pop('counter') #clear specific key
-    #     session.clear() #clear all keys
-    #     print(session['reset'])
-
-    print("added")
     session['counter'] += 1
-
     return redirect('/')
 
 @app.route('/reset' , methods=['POST'])
 def reset_count():
-    print("reseted")
+    # session.pop('counter') #clear specific key
+    # session.clear() #clear all keys
+    #session.clear()
+    session.pop('counter')
+    return redirect('/')
+
+@app.route('/hard_reset' , methods=['POST'])
+def hard_reset_count():
     session.clear()
+    return redirect('/')
+
+@app.route('/inc' , methods=['POST'])
+def num_count():
+    count = request.form['count']
+    session['increment'] = count
+    session['counter'] += int(count)-1
     return redirect('/')
 
 if __name__ == "__main__":
