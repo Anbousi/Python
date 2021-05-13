2e20e7a87920698c0da3b35be7b733d070a13aad
Mahmoud Anbousi <anbousi@gmail.com>
2021-05-13 13:58:28 +0300

    counter flask modified

--- python_stack/flask/flask_fundamentals/counter/server.py
+++ python_stack/flask/flask_fundamentals/counter/server.py
@@ -1,11 +1,14 @@
 from flask import Flask , render_template , redirect , request , session
 app = Flask(__name__)
 app.secret_key = "Secret key"
-count = 1
+# count = 1
 @app.route('/')
 def index():
     if 'real_counter' and 'counter' in session:
-        session['counter'] += count
+        # session['counter'] += count
+        if 'count' not in session:
+            session['count'] = 1
+        session['counter'] += 1
         session['real_counter'] += 1
 
     else:
@@ -14,12 +17,23 @@ def index():
             session['real_counter'] = 1
         session['increment'] = 1
         
+        
     return render_template("index.html" , count = session['counter'] , count1 = session['increment'] , real_count = session['real_counter'])
 
 
 @app.route('/counter' , methods=['POST'])
 def counters():
     session['counter'] += 1
+    return redirect('/')
+
+@app.route('/inc' , methods=['POST'])
+def num_count():
+    # count = request.form['count']
+    session['count'] = request.form['count']
+    if int(session['count']) < 0:
+        session['count'] = 0
+    session['increment'] = session['count']
+    session['counter'] += int(session['count'])-1
     return redirect('/')
 
 @app.route('/reset' , methods=['POST'])
@@ -35,12 +49,7 @@ def hard_reset_count():
     session.clear()
     return redirect('/')
 
-@app.route('/inc' , methods=['POST'])
-def num_count():
-    count = request.form['count']
-    session['increment'] = count
-    session['counter'] += int(count)-1
-    return redirect('/')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
