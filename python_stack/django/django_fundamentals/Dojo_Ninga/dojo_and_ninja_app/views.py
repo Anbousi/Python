2768caba030a86ab361cce8aea1a052844fc400c
Mahmoud Anbousi <anbousi@gmail.com>
2021-05-24 00:26:39 +0300

    Dojo and ninjas

--- python_stack/django/django_fundamentals/Dojo_Ninga/dojo_and_ninja_app/views.py
+++ python_stack/django/django_fundamentals/Dojo_Ninga/dojo_and_ninja_app/views.py
@@ -11,11 +11,15 @@ def main(request):
     return render(request, 'index.html', context)
 
 def add_dojo(request):
-    if request.method == "POST":
-        Dojos.objects.create(name = request.POST['f_name'] , city =request.POST['city'] , state = request.POST['state'])
+    Dojos.objects.create(name = request.POST['f_name'] , city =request.POST['city'] , state = request.POST['state'])
     return redirect('/')
 
 def add_ninja(request):
     if request.method == "POST":
         Ninjas.objects.create(first_name = request.POST['first_name'] , last_name =request.POST['last_name'] , dojo = Dojos.objects.get(name=request.POST['dojo'])) 
+    return redirect('/')
+
+def delete(request):
+    if request.method == "POST":
+        Dojos.objects.get(name=request.POST['delete']).delete()
     return redirect('/')
